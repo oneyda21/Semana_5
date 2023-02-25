d687aad7bbddc8e8c05e6420aa46d3d996c6d166
oneyda21 <124007400+oneyda21@users.noreply.github.com>
2023-02-24 22:53:23 -0600

    Ejercicio Semana5

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace semana5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rango_ini;
+            int rango_fin;
+            int parCount = 0; 
+
+            Console.Write("Ingrese un numero de rango inicial: ");
+            rango_ini = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese un numero de rango final: ");
+            rango_fin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Los números pares entre " + rango_ini + " y " + rango_fin + " son:");
+
+            for (int i = rango_ini; i <= rango_fin; i++)
+            { 
+                if (i % 2 == 0)
+                { Console.WriteLine(i.ToString());
+                    parCount++;
+                }
+
+                else
+                {
+                   
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
+
